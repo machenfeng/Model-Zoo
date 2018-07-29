e3e4968f1135dc84f5a20384e64b4554072a0a62
machenfeng <machenfeng@msn.cn>
2018-07-29 22:33:25 +0800

    update

--- STPR.py
+++ STPR.py
@@ -28,63 +28,76 @@ class Model(nn.Module):
                                   nn.MaxPool2d(kernel_size=(sl - ks + 1 + ps * 2, 1)))
         
         self.m = nn.Parameter(torch.Tensor(oc, oc))
-        self.m.data.uniform_(-0.1, 0.1)
         
         self.fc = nn.Sequential(nn.Linear(2 * oc + 1, hs), 
                                 nn.BatchNorm1d(num_features=hs), 
                                 nn.ReLU(), 
                                 nn.Linear(hs, 2))
+        """ params init """
+        for m in self.modules():
+            
+            if isinstance(m, nn.Embedding):
+                m.weight.data.normal_(0, 0.01)
+                
+            if isinstance(m, nn.Parameter):
+                m.data.normal_(0, 0.01)
+                
+            if isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.fill_(0.01)
+            
+            if isinstance(m, nn.Conv2d):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.fill_(0.01)          
 
-    
-    def forward(self, q, a):
+    def forward(self, x1, x2):
         
-        bs = q.size(0)
+        bs = x1.size(0)
         
         # (bs, sl) -> (bs, 1, sl, ed)
-        q = self.embed(q).unsqueeze(1)
-        a = self.embed(a).unsqueeze(1)
+        x1 = self.embed(x1).unsqueeze(1)
+        x2 = self.embed(x2).unsqueeze(1)
         
         # formula (1)
         # (bs, 1, sl, ed) -> (bs, oc)
-        q = self.conv(q).view(bs, -1)
-        a = self.conv(a).view(bs, -1)
+        x1 = self.conv(x1).view(bs, -1)
+        x2 = self.conv(x2).view(bs, -1)
         
         # formula (2)
         # sim.size() = (bs, 1)
-        q_ = q.unsqueeze(1)
-        a_ = a.unsqueeze(1).transpose(2, 1)
+        x1_ = x1.unsqueeze(1)
+        x2_ = x2.unsqueeze(1).transpose(2, 1)
         m = self.m.expand(bs, -1, -1)
-        sim = torch.bmm(q_, m)
-        sim = torch.bmm(sim, a_).squeeze(2)
+        sim = torch.bmm(x1_, m)
+        sim = torch.bmm(sim, x2_).squeeze(2)
         
         # (bs, 2 * oc + 1)
-        out = torch.cat([q, sim, a], 1)
+        out = torch.cat([x1, sim, x2], 1)
 
         # (bs, 2 * oc + 1) -> (bs, hs)
         logits = self.fc(out)
         
         return logits
-
-
-def eval(model, dataloader, lossfunc):
-
-    y_pred = []
-    y_true = []
-    total_loss = 0
-    batch_count = len(dataloader)
     
-    for (q, a, _, __, target) in dataloader:
+    @staticmethod
+    def evaluate(model, dataloader, lossfunc):
 
-        q = q.to(device)
-        a = a.to(device)
-        target = target.view(-1).to(device)
-        
-        logits = model(q, a)
-        predict = logits.max(1)[1]
-        loss = lossfunc(logits, target)
-        total_loss += loss.item()
+        y_pred = []
+        y_true = []
+        total_loss = 0
+        batch_count = len(dataloader)
 
-        y_true += target.tolist()
-        y_pred += predict.tolist()
-    
-    return y_true, y_pred, total_loss / batch_count
+        for (x1, x2, _, __, target) in dataloader:
+
+            x1, x2 = x1.to(device), x2.to(device)
+            target = target.view(-1).to(device)
+
+            logits = model(x1, x2)
+            predict = logits.max(1)[1]
+            loss = lossfunc(logits, target)
+            total_loss += loss.item()
+
+            y_true += target.tolist()
+            y_pred += predict.tolist()
+
+        return y_true, y_pred, total_loss / batch_count
